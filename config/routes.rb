509c242db5ac07c6b36f96a51e2f68e5ac8eb8ad
Bryce Kerley <bkerley@brycekerley.net>
2014-08-21 11:57:01 -0400

    add route to status#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'statuses', to: 'statuses#index', page: 1
   get 'statuses/page/:page', to: 'statuses#index'
 
+  post 'statuses', to: 'statuses#create'
+  
   get 'statuses/show'
 
   get 'statuses/new'
